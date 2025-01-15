abbcce4bb453a807dd77e3e9b131a85e512fc17c
mesakhbesta <mesakhbesta@gmail.com>
2025-01-15 16:08:17 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -11,14 +11,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
-
 
 media_urls = {
     "kompas": "https://www.kompas.com/",
